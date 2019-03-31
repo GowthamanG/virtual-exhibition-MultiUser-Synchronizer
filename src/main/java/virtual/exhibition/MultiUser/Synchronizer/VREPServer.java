2b6614c2a8badbb4dd7aa96949d99793b943287a
Gowthaman Gobalasingam <gowthaman.gobalasingam@stud.unibas.ch>
2019-04-01 00:07:40 +0200

    variable for port number

--- src/main/java/virtual/exhibition/MultiUser/Synchronizer/VREPServer.java
+++ src/main/java/virtual/exhibition/MultiUser/Synchronizer/VREPServer.java
@@ -7,12 +7,18 @@ import java.io.IOException;
 
 public class VREPServer {
 
+    private static final int port = 8080;
+
     public static void main(String[] args) throws IOException, InterruptedException{
 
-        Server server = ServerBuilder.forPort(8080).addService(new MultiUserSyncImpl()).build();
+        Server server = ServerBuilder.forPort(port).addService(new MultiUserSyncImpl()).build();
 
         server.start();
+        System.out.println("Server started, listeing on port: " + port);
 
+        if(!server.getListenSockets().isEmpty())
+            System.out.println("Server is conntected to: " + server.getListenSockets().toString());
         server.awaitTermination();
     }
 }
+
